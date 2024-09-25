afa80d2089533ae879aba3d3c366e7186a906228
Malachi <mpeel9938@gmail.com>
2024-09-24 23:15:03 -0700

    CSCI 167 : Notebook6

--- CSCI30/excersises/primeNums.js
+++ CSCI30/excersises/primeNums.js
@@ -1,20 +1,48 @@
 function addRow(){
     let user_input = document.getElementById("user_input");
     let table = document.getElementById("prime_table");
-    let rowCount = table.rows.length;
-    let row = table.insertRow(rowCount);
 
-    row.insertCell(0).innerHTML= rowCount;
-    row.insertCell(1).innerHTML= calculatePrimeNums(user_input);
+    deleteTable()
+
+    for (let i = 0; i < user_input.value; i++){
+
+        let rowCount = table.rows.length;
+        let row = table.insertRow(rowCount);
+        row.insertCell(0).innerHTML= rowCount;
+        row.insertCell(1).innerHTML= calculatePrimeNums(rowCount);
+
+    }
 
 }
 
-function deleteRow(obj){
+function calculatePrimeNums(index){
+
+    if(index == 1){
+        return 2
+    }
+    else if(index == 2){
+        return 3
+    }
+    else if(index==3){
+        return 5
+    }
+    else if(index==4){
+        return 7
+    }
+    else if(index==5){
+        return 11
+    }
+    else
+        return 6 * index -1
+    
+
+}
+
+function deleteTable(){
     let table = document.getElementById("prime_table");
-    let index = obj.parentNode.rowIndex; 
-    table.deleteRow(index);
-}
 
-function calculatePrimeNums(){
+    while(table.rows.length > 1){
+        table.deleteRow(1)
+    }
 
 }
