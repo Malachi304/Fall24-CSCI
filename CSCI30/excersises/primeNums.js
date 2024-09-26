1ea00fcf0cf1d27a073b802f793b6f6879b332f5
Malachi <mpeel9938@gmail.com>
2024-09-26 08:08:30 -0700

    CSCI 130: Prime number table generator by index, completed

--- CSCI30/excersises/primeNums.js
+++ CSCI30/excersises/primeNums.js
@@ -2,12 +2,21 @@ function addRow(){
     let user_input = document.getElementById("user_input");
     let table = document.getElementById("prime_table");
 
+
+    // Delete the table (excluding headers, see function) when adding new content
     deleteTable()
 
+    // Number of primes the user wants to generate
     for (let i = 0; i < user_input.value; i++){
 
+        // Get current row index
         let rowCount = table.rows.length;
+        console.log(rowCount);
+        // Insert new row at next avalible index
         let row = table.insertRow(rowCount);
+
+        // First cell = index (row count)
+        // Second cell = prime number at that index
         row.insertCell(0).innerHTML= rowCount;
         row.insertCell(1).innerHTML= calculatePrimeNums(rowCount);
 
@@ -16,28 +25,44 @@ function addRow(){
 }
 
 function calculatePrimeNums(index){
+    // Array of prime numbers
+    let primes = [];
 
-    if(index == 1){
-        return 2
-    }
-    else if(index == 2){
-        return 3
-    }
-    else if(index==3){
-        return 5
-    }
-    else if(index==4){
-        return 7
-    }
-    else if(index==5){
-        return 11
-    }
-    else
-        return 6 * index -1
-    
+    // Current number. Prime numbers start at 2
+    let currentNum = 2; 
 
+    // While length of prime number array is less than index of current row
+    // We want the last index of the array to be returned
+    while(primes.length < index){
+        // Check if the current number is prime or not
+        if(is_prime(currentNum)){
+            primes.push(currentNum)
+        }
+        currentNum ++; 
+    }
+
+    return primes[primes.length - 1]; 
 }
 
+function is_prime(num){
+
+    // Prime numbers are always greater than 1
+    if(num <= 1){
+        return false
+    }
+
+    // If a number is divisible by a number GREATER THAN or EQUAL TO it's own square root,
+    // then it is true that it is also divisible by a smaller number, 
+    // which if true, means the number is divisible by more than 1, and itself. 
+    for(let i = 2; i <= Math.sqrt(num); i++){
+        if(num % i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+// Deletes all table rows after index 1 (Header)
 function deleteTable(){
     let table = document.getElementById("prime_table");
 
